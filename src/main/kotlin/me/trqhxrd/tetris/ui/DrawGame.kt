770f84481daa155f795c7b7a9749bf11368ac021
Trqhxrd <53657159+Trqhxrd@users.noreply.github.com>
2022-09-23 11:03:27 +0200

    Started adding GUI options.

---
+++ src/main/kotlin/me/trqhxrd/tetris/ui/DrawGame.kt
@@ -0,0 +1,22 @@
+package me.trqhxrd.tetris.ui
+
+import me.trqhxrd.tetris.ui.GUI.TILE_SIZE
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.Graphics2D
+import javax.swing.JLabel
+
+object DrawGame : JLabel() {
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+        if (g !is Graphics2D) return
+
+        g.color = Color.LIGHT_GRAY
+        repeat(10) { x ->
+            repeat(18) { y -> g.drawRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE) }
+        }
+
+
+        this.repaint()
+    }
+}
--- src/main/kotlin/me/trqhxrd/tetris/ui/GUI.kt
+++ src/main/kotlin/me/trqhxrd/tetris/ui/GUI.kt
@@ -2,11 +2,14 @@ package me.trqhxrd.tetris.ui
 
 import me.trqhxrd.tetris.io.KeyHandler
 import java.awt.Dimension
+import java.awt.Rectangle
 import javax.swing.JFrame
+import javax.swing.JLabel
 
 object GUI {
     const val WIDTH = 320
     const val HEIGHT = 576
+    const val TILE_SIZE = 32
 
     lateinit var jf: JFrame
         private set
@@ -19,8 +22,17 @@ object GUI {
         jf.isResizable = false
         jf.layout = null
         jf.addKeyListener(KeyHandler())
-
         jf.requestFocus()
+
+        this.setupDraw(DrawGame, 0, 0, WIDTH, HEIGHT)
+        this.setupDraw(DrawInterface, WIDTH + 1, 1, WIDTH, HEIGHT)
+
         jf.isVisible = true
+    }
+
+    private fun setupDraw(draw: JLabel, x: Int, y: Int, width: Int, height: Int) {
+        draw.bounds = Rectangle(x, y, width, height)
+        draw.isVisible = true
+        this.jf.add(draw)
     }
 }
